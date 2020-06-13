03a256f0176367497fe6c9e16a67fcd7b4a2c2d7
brianlunch <blynch5@tcd.ie>
2020-06-13 15:42:35 +0100

    Updates

---
+++ precache-manifest.9519b169015a2c9372cc170d3f028ccf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf6b21fc8c40170eef300988623062b1",
+    "url": "/menu-view/index.html"
+  },
+  {
+    "revision": "cdbfdaa604dd8d0a342e",
+    "url": "/menu-view/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2732162a66347be58bba",
+    "url": "/menu-view/static/css/main.08e8f2fd.chunk.css"
+  },
+  {
+    "revision": "cdbfdaa604dd8d0a342e",
+    "url": "/menu-view/static/js/2.0cd8c449.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/menu-view/static/js/2.0cd8c449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2732162a66347be58bba",
+    "url": "/menu-view/static/js/main.3120fb40.chunk.js"
+  },
+  {
+    "revision": "8d1eaf71febe0ea04cf3",
+    "url": "/menu-view/static/js/runtime-main.caafd5ac.js"
+  },
+  {
+    "revision": "1753890292117a855ab2ec8c4e15ddc1",
+    "url": "/menu-view/static/media/PavMenu.17538902.PNG"
+  },
+  {
+    "revision": "38421ac5e114265a51a8114543462274",
+    "url": "/menu-view/static/media/ScanImage.38421ac5.PNG"
+  },
+  {
+    "revision": "e7800a41cfd42729d879ccc6819b8f6e",
+    "url": "/menu-view/static/media/ScanSheetLabels.e7800a41.PNG"
+  },
+  {
+    "revision": "c8b9f8e5750bd02488fc5b8ce21cc0a7",
+    "url": "/menu-view/static/media/TapTagImage.c8b9f8e5.PNG"
+  }
+]);
