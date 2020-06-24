96b644dc063ba04fb0ce688f35d88658439213ea
brianlunch <blynch5@tcd.ie>
2020-06-24 04:11:42 +0100

    Added our team and links

---
+++ build/precache-manifest.5bb8d8325fd5e243a28c02e62eee1db8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c99bbb733d5f5426b757dde1dfbea33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "528e2b5c9a86b1444fef",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "00646f9c1e7a27163cb4",
+    "url": "/static/css/main.91150ee3.chunk.css"
+  },
+  {
+    "revision": "528e2b5c9a86b1444fef",
+    "url": "/static/js/2.fee39041.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fee39041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00646f9c1e7a27163cb4",
+    "url": "/static/js/main.eb4a16c9.chunk.js"
+  },
+  {
+    "revision": "0040949cf2beb8679f99",
+    "url": "/static/js/runtime-main.b5c3ad86.js"
+  },
+  {
+    "revision": "1753890292117a855ab2ec8c4e15ddc1",
+    "url": "/static/media/PavMenu.17538902.PNG"
+  },
+  {
+    "revision": "c131b9a96eea45c41acd4ff0fb2e955d",
+    "url": "/static/media/Sample1.c131b9a9.PNG"
+  },
+  {
+    "revision": "38421ac5e114265a51a8114543462274",
+    "url": "/static/media/ScanImage.38421ac5.PNG"
+  },
+  {
+    "revision": "e7800a41cfd42729d879ccc6819b8f6e",
+    "url": "/static/media/ScanSheetLabels.e7800a41.PNG"
+  },
+  {
+    "revision": "c8b9f8e5750bd02488fc5b8ce21cc0a7",
+    "url": "/static/media/TapTagImage.c8b9f8e5.PNG"
+  },
+  {
+    "revision": "ae9dd1ea5fe091de0213cfdffda96b99",
+    "url": "/static/media/emenu.ae9dd1ea.png"
+  }
+]);
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import Navbar from '../Components/Navbar'
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css'
-
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faGlobe, faEnvelope} from '@fortawesome/free-solid-svg-icons'
+ 
 class Contact extends React.Component{
    constructor(props) {
       super(props)
@@ -23,28 +24,21 @@ return (
            <div className="col-xs-12 col-sm-12 col-lg-6">
                <h2 className="text-blue">Contact Us</h2>
                <p>
-                   Please fill in your request below:
+                   Please Contact Us:
                </p>
                <form role="form" method="post" id="reused_form">
                    <div className="row">
 
                    </div>
+                   
                    <div className="row">
-                       <div className="col-xs-12 col-sm-12 col-lg-6 form-group">
-                           <label htmlFor="comments">
-                               Comments:</label>
-                           <textarea className="form-control" type="textarea" name="comments" id="comments"
-                                     placeholder="Your Comments" maxLength="6000" rows="7"></textarea>
-                       </div>
-                   </div>
-                   <div className="row">
-                       <div className="col-sm-6 form-group">
+                       <div className="col-sm-12 form-group">
                            <label htmlFor="name">
                                Your Name:</label>
                            <input type="text" className="form-control" id="name" name="name" required>
                            </input>
                        </div>
-                       <div className="col-sm-6 form-group">
+                       <div className="col-sm-12 form-group">
                            <label htmlFor="email">
                                Email:</label>
                            <input type="email" className="form-control" id="email" name="email" required>
@@ -53,13 +47,82 @@ return (
                    </div>
 
                    <div className="row">
-                       <div className="col-sm-12 form-group">
-                           <button type="submit" className="btn btn-lg btn-primary btn-block">Send →</button>
+                       <div className="col-xs-12 col-sm-12 col-lg-12 form-group">
+                           <label htmlFor="comments">
+                               Comments:</label>
+                           <textarea className="form-control" type="textarea" name="comments" id="comments"
+                                     placeholder="Your Comments" maxLength="6000" rows="7"></textarea>
+                       </div>
+                   </div>
+
+                   <div className="row">
+                       <div className="col-sm-12 text-center form-group">
+                       <button type="submit" class="btn btn-outline-primary">Send →</button>
                        </div>
                    </div>
 
                </form>
 
+           </div>
+
+           <div className="col-xs-12 col-sm-12 col-lg-1 text-center">
+              </div>
+           <div className="col-xs-12 col-sm-12 col-lg-5 text-center">
+<br/><br/><br/><br/><br/><br/>
+<h1 className ="text-left"> Our Team</h1><br/>
+              <h2>Brian Lynch</h2>
+              <h5>4th Year Computer Science Student & Founder</h5>
+           <div className="row text-center justify-content-center">
+                            <div className="zoom col-2 text-center">
+                  <a target="_blank" href="http://www.github.com/brianlunch">
+                    <h1 className="text-blue"><FontAwesomeIcon icon={faGithub} /></h1>
+                  </a>
+                </div>
+                <div className="zoom col-2 text-center">
+                  <a target="_blank" href="mailto:blynch5@tcd.ie">
+                    <h1 className="text-blue"><FontAwesomeIcon icon={faEnvelope} /></h1>
+                  </a>
+                </div>
+                <div className="zoom col-2">
+                  <a target="_blank" href="http://www.brianlunch.github.io/Personal">
+                    <h1 className="text-blue"><FontAwesomeIcon icon={faGlobe} /></h1>
+                  </a>
+                </div>
+                <div className="zoom col-2 text-center">
+                  <a target="_blank" href="https://www.linkedin.com/in/brian-lynch-8a1095155/">
+
+                <h1 className="text-blue"><FontAwesomeIcon icon={faLinkedin} /></h1>
+                  </a>
+                </div>
+                
+                </div>
+<br/><br/>
+<h2>Liam Egan</h2>
+              <h5>4th Year Computer Science Student & Founder</h5>
+           <div className="row text-center justify-content-center">
+                            <div className="zoom col-2 text-center">
+                  <a target="_blank" href="http://www.github.com/EazyEgan">
+                    <h1 className="text-blue"><FontAwesomeIcon icon={faGithub} /></h1>
+                  </a>
+                </div>
+                <div className="zoom col-2 text-center">
+                  <a target="_blank" href="mailto:eganl7@tcd.ie">
+                    <h1 className="text-blue"><FontAwesomeIcon icon={faEnvelope} /></h1>
+                  </a>
+                </div>
+                <div className="zoom col-2">
+                  <a target="_blank" href="http://www.github.com/EazyEgan">
+                    <h1 className="text-blue"><FontAwesomeIcon icon={faGlobe} /></h1>
+                  </a>
+                </div>
+                <div className="zoom col-2 text-center">
+                  <a target="_blank" href="https://www.linkedin.com/in/brian-lynch-8a1095155/">
+
+                <h1 className="text-blue"><FontAwesomeIcon icon={faLinkedin} /></h1>
+                  </a>
+                </div>
+                
+                </div>
            </div>
        </div>
    </div>
