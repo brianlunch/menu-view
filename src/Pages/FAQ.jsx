f20e72881c9260a70a2b9614271d3c9a642f4ed4
brianlunch <blynch5@tcd.ie>
2020-06-25 22:39:02 +0100

    Contact form and FAQ

--- src/Pages/FAQ.jsx
+++ src/Pages/FAQ.jsx
@@ -17,7 +17,7 @@ return (
 <div className=" slide-in-fwd-center text-left">
    <Navbar history={this.state.history} />
    <div class="row contact">
-      <div className="col-6">
+      <div className="col-xs-12 col-sm-12 col-lg-6">
           <h2 className="text-blue">Frequently Asked Questions</h2>
           <p>If you have any other questions please feel free to <Link className="link" to="contact-us">contact us</Link>.</p>
          <br/>
